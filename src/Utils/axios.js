cb8125d344fc063568d3ae628affc02cbfbd8019
Gulshan gupta <abhinavg90834@gmail.com>
2022-01-29 20:45:38 +0530

    url  added

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://social1server.herokuapp.com/",
+    baseURL: "https://social1server.herokuapp.com/api",
 });
 
 export default instance
